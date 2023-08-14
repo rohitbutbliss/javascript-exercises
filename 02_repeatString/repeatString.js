865ff052de556d1da4df22ccb034cb4807e3691a
bliss <bliss@Rohits-MacBook-Air.local>
2023-08-14 11:54:09 +0530

    Completed first two exercise of js testing assignment - completed a program to repeat character n number of times along with edge cases - all testcases are getting passed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    if (num < 0) return "ERROR";
+    let currentString = "";
+    for (let i = 0; i < num; i++) currentString = currentString + string;
+    return currentString;
 };
 
 // Do not edit below this line
